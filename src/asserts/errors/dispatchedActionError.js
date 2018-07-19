03adb15abc5cd70357a9088797e01fa8c62e41ba
Samuel Marks <sam@fooji.com>
2018-07-19 10:48:08 -0400

    Support circular references.

--- src/asserts/errors/dispatchedActionError.js
+++ src/asserts/errors/dispatchedActionError.js
@@ -1,8 +1,10 @@
+import stringify from 'json-stringify-safe';
+
 function dispatchedActionError(dispatchedActions, expectedActions, action) {
   return new Error(
-    `Action ${JSON.stringify(action)} was dispatched when it was unexpected.\n` +
-    `Actions expected to be not dispatched: ${JSON.stringify(expectedActions)}\n` +
-    `Actual dispatched actions: ${JSON.stringify(dispatchedActions)}`
+    `Action ${stringify(action)} was dispatched when it was unexpected.\n` +
+    `Actions expected to be not dispatched: ${stringify(expectedActions)}\n` +
+    `Actual dispatched actions: ${stringify(dispatchedActions)}`
   );
 }
 
--- src/asserts/errors/notDispatchedActionError.js
+++ src/asserts/errors/notDispatchedActionError.js
@@ -1,8 +1,10 @@
+import stringify from 'json-stringify-safe';
+
 function notDispatchedActionError(dispatchedActions, expectedActions, action) {
   return new Error(
-    `Action ${JSON.stringify(action)} was not dispatched when it was expected.\n` +
-    `Actions expected to be dispatched: ${JSON.stringify(expectedActions)}\n` +
-    `Actual dispatched actions: ${JSON.stringify(dispatchedActions)}`
+    `Action ${stringify(action)} was not dispatched when it was expected.\n` +
+    `Actions expected to be dispatched: ${stringify(expectedActions)}\n` +
+    `Actual dispatched actions: ${stringify(dispatchedActions)}`
   );
 }
 
